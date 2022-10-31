34562e12ff877cc6cbf67eb94dfc0e7c5c25164d
belka141 <belo4kalala1441@gmail.com>
2022-10-31 14:17:19 +0300

    initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,5 @@
-package org.example;
-
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world with Database)");
     }
 }
